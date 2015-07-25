8926e4d7f6c30f61f153cd028a9a762036fdf215
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-07-25 09:45:38 -0400

    Cambiando a un protocolo basado en RE

--- server.py
+++ server.py
@@ -12,6 +12,20 @@ SERVER_BUFFER_SIZE = 1024
 
 MESSAGE_LOGGER = 'gpstracker.log'
 
+imei_dict = {}
+
+def p_load(imei, parm) {
+	if imei in imei_dict:
+		return False
+	else:
+		imei_dict[imei] = True
+	return 'LOAD'
+}
+
+TRACKER_PROTOCOL = {
+	r'##,imei:(\d+),(\w+);': p_load,
+}
+
 TRACKER_MESSAGE_CHUNKS = "<2s11s8s11s6s1s10s1s10s5s6s6s8s1s8s1s"
 TRACKER_MESSAGE_KEYS = ['protocol_version',
                         'device_id',
@@ -41,14 +55,20 @@ class GpsHandler(socketserver.BaseRequestHandler):
             data = self.request.recv(SERVER_BUFFER_SIZE)
             terminate = data == b''
             logging.info("Recived:{0}".format(data))
+
             if not terminate and len(data) == tracker_message_chunks.size:
                 data_struct = dict(zip(TRACKER_MESSAGE_KEYS,
                                        tracker_message_chunks.unpack(data)))
                 logging.info("Struct:{0}".format(data_struct))
+            else:
+                for kre, vfunc in TRACKER_PROTOCOL.items():
+			kre.match(data)
+
         logging.info("Disconnection:{0}".format(self.server.socket))
 
 
 if __name__ == '__main__':
     logging.basicConfig(filename=MESSAGE_LOGGER, level=logging.DEBUG)
     server = socketserver.TCPServer((SERVER_HOST, SERVER_PORT), GpsHandler)
+    logging.info("Start serving:{0}".format(server))
     server.serve_forever()
