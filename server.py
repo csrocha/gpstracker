bb68005059752e1f3c7cb4760ba39a939a3abf6a
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2015-07-26 16:32:23 -0300

    [FIX] Primeros pasos del protocolo implementado: LOAD y ON

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import socket
 import socketserver
 import logging
 import struct
+import re
+import datetime
 
 SERVER_HOST = socket.gethostname()
 SERVER_PORT = 8000
@@ -14,17 +16,26 @@ MESSAGE_LOGGER = 'gpstracker.log'
 
 imei_dict = {}
 
-def p_load(imei, parm) {
-	if imei in imei_dict:
-		return False
-	else:
-		imei_dict[imei] = True
-	return 'LOAD'
-}
 
-TRACKER_PROTOCOL = {
-	r'##,imei:(\d+),(\w+);': p_load,
-}
+def p_load(imei, parm):
+    imei_dict[imei] = datetime.datetime.now()
+    return 'LOAD'
+
+
+def p_heartbeat(imei):
+    imei_dict[imei] = datetime.datetime.now()
+    return 'ON'
+
+TRACKER_PROTOCOL_RE = [
+    (r'?P<p_load_0>##,imei:(?P<imei_0>\d+),(?P<parm_0>\w+);', p_load),
+    (r'?P<p_heartbeat_1>(?P<imei_1>\d+)', p_heartbeat),
+]
+
+TRACKER_PROTOCOL = re.compile(r'(' +
+                              r')|('.join(_re
+                                          for _re, _f in TRACKER_PROTOCOL_RE) +
+                              r')')
+
 
 TRACKER_MESSAGE_CHUNKS = "<2s11s8s11s6s1s10s1s10s5s6s6s8s1s8s1s"
 TRACKER_MESSAGE_KEYS = ['protocol_version',
@@ -53,22 +64,37 @@ class GpsHandler(socketserver.BaseRequestHandler):
         logging.info("Connection:{0}".format(self.server.socket))
         while not terminate:
             data = self.request.recv(SERVER_BUFFER_SIZE)
+            str_data = str(data, 'utf8')
             terminate = data == b''
             logging.info("Recived:{0}".format(data))
 
-            if not terminate and len(data) == tracker_message_chunks.size:
-                data_struct = dict(zip(TRACKER_MESSAGE_KEYS,
-                                       tracker_message_chunks.unpack(data)))
-                logging.info("Struct:{0}".format(data_struct))
+            mat_data = TRACKER_PROTOCOL.match(str_data)
+            if mat_data:
+                tokens = {k: v
+                          for k, v in mat_data.groupdict().items()
+                          if v is not None}
+                idx = int([k.split('_')[-1] for k in tokens][0])
+                _re, _f = TRACKER_PROTOCOL_RE[idx]
+                args = {k.split('_')[0]:v
+                        for k,v in tokens.items()
+                        if 'p_' != k[:2]}
+                r = _f(**args)
+                logging.info("Executed:{0}:{1}->{2}".format(_f, args, r))
+                self.request.sendall(bytes(r + '\n', 'utf8'))
             else:
-                for kre, vfunc in TRACKER_PROTOCOL.items():
-			kre.match(data)
-
+                logging.info("Not implemented:{0}".format(data))
+                self.request.sendall(bytes('\n', 'utf8'))
         logging.info("Disconnection:{0}".format(self.server.socket))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename=MESSAGE_LOGGER, level=logging.DEBUG)
-    server = socketserver.TCPServer((SERVER_HOST, SERVER_PORT), GpsHandler)
-    logging.info("Start serving:{0}".format(server))
-    server.serve_forever()
+    try:
+        logging.basicConfig(filename=MESSAGE_LOGGER, level=logging.DEBUG)
+        server = socketserver.TCPServer((SERVER_HOST, SERVER_PORT), GpsHandler)
+        logging.info("Start serving:{0}".format(server))
+        server.serve_forever()
+    except KeyboardInterrupt:
+        logging.info('Pressed Ctrl+C. Stopping.')
+        server.socket.close()
+
+#
